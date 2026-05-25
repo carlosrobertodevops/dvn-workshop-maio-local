4bf531d589ce3b9090e005dc9a4dfdb8d573d901
“carlosrobertodevops” <carlos.roberto.devops@gmail.com>
2026-05-25 18:35:17 -0300

    first commit

---
+++ env.sh
@@ -0,0 +1,23 @@
+#!bash
+# export AWS_REGION=$(aws configure get region --profile default)
+# export AWS_ACCESS_KEY_ID=$(aws configure get aws_access_key_id --profile default)
+# export AWS_SECRET_ACCESS_KEY=$(aws configure get aws_secret_access_key --profile default)
+# export AWS_API_MCP_PROFILE_NAME=default
+
+
+export AWS_REGION=us-east-1
+export AWS_ACCESS_KEY_ID=test
+export AWS_SECRET_ACCESS_KEY=test
+export AWS_DEFAULT_REGION=us-east-1
+export AWS_API_MCP_PROFILE_NAME=default
+
+export LOCALSTACK_HOST=http://localhost
+export EDGE_PORT=4566
+export AWS_ENDPOINT_URL=http://localhost:4566
+export LOCALSTACK_HOSTNAME=localhost
+export LAMBDA_RUNTIME=nodejs20.x
+export SERVERLESS_DEPLOYMENT_BUCKET=testbucket
+
+set -a
+source .env
+set +a
